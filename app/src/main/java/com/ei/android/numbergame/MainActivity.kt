7c5b9206fa44682279b4695dfb5d34ee8f17a29c
Elchyn Iskenderov <elchiniskanderov@gmail.com>
2022-10-05 21:30:31 +0300

    Initial commit

---
+++ app/src/main/java/com/ei/android/numbergame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ei.android.numbergame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
